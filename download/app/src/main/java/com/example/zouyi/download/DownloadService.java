979c8cc688570d4d788d6be501dfc0081c44316f
zouyi9807 <1530193044@qq.com>
2020-01-08 04:45:32 +0800

    download

---
+++ download/app/src/main/java/com/example/zouyi/download/DownloadService.java
@@ -0,0 +1,125 @@
+package com.example.zouyi.download;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class DownloadService extends Service {
+    private DownloadTask downloadTask;
+    private String downloadUrl;
+
+    private DownloadListener downloadListener = new DownloadListener() {
+        @Override
+        public void onProgress(int progress) {
+            getNotificationManger().notify(1, getNotification("Downloading...", progress));
+        }
+
+        @Override
+        public void onSuccess() {
+            downloadTask = null;
+            stopForeground(true);
+            getNotificationManger().notify(1,getNotification("Download success!",-1));
+            Toast.makeText(DownloadService.this,"Download success!",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onFailed() {
+            downloadTask = null;
+            stopForeground(true);
+            getNotificationManger().notify(1,getNotification("Download failed!",-1));
+            Toast.makeText(DownloadService.this,"Download failed!",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPaused() {
+            downloadTask=null;
+            Toast.makeText(DownloadService.this,"Paused.",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCanceled() {
+            downloadTask=null;
+            stopForeground(true);
+            Toast.makeText(DownloadService.this,"Canceled.",Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    public DownloadService() {
+    }
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        //throw new UnsupportedOperationException("Not yet implemented");
+        return downloadBinder;
+    }
+
+    private DownloadBinder downloadBinder = new DownloadBinder();
+
+    class DownloadBinder extends Binder {
+
+        public void startDownload(String url) {
+            if (downloadTask == null) {
+                downloadUrl = url;
+                downloadTask = new DownloadTask(downloadListener);
+                downloadTask.execute(downloadUrl);
+                startForeground(1, getNotification("Downloading...", 0));
+                Toast.makeText(DownloadService.this, "Downloading...", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        public void pauseDownload() {
+            if (downloadTask != null) {
+                downloadTask.pauseDownload();
+            }
+        }
+
+        public void cancelDownload() {
+            if (downloadTask != null) {
+                downloadTask.cancelDownload();
+            } else {
+                if (downloadUrl != null) {
+                    String fileName = downloadUrl.substring(downloadUrl.lastIndexOf("/"));
+                    String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+                    File file = new File(directory + fileName);
+                    if (file.exists()) {
+                        file.delete();
+                    }
+                    getNotificationManger().cancel(1);
+                    stopForeground(true);
+                    Toast.makeText(DownloadService.this, "Canceled", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private NotificationManager getNotificationManger() {
+        return (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    private Notification getNotification(String title, int progress) {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        builder.setContentIntent(pendingIntent);
+        builder.setContentTitle(title);
+        if (progress >= 0) {
+            builder.setContentText(progress + "%");
+            builder.setProgress(100, progress, false);
+        }
+        return builder.build();
+    }
+}
---
+++ download/app/src/main/java/com/example/zouyi/download/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.zouyi.download;
+
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private DownloadService.DownloadBinder downloadBinder;
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder = (DownloadService.DownloadBinder) service;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button startDownload = findViewById(R.id.start_download);
+        Button pauseDownload = findViewById(R.id.pause_download);
+        Button cancelDownload = findViewById(R.id.cancel_download);
+        startDownload.setOnClickListener(this);
+        pauseDownload.setOnClickListener(this);
+        cancelDownload.setOnClickListener(this);
+        Intent intent = new Intent(this, DownloadService.class);
+        startService(intent);
+        bindService(intent, serviceConnection, BIND_AUTO_CREATE);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 1);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (downloadBinder == null) {
+            return;
+        }
+        switch (v.getId()) {
+            case R.id.start_download:
+                String url = "https://qd.myapp.com/myapp/qqteam/AndroidQQ/mobileqq_android.apk";
+                downloadBinder.startDownload(url);
+                break;
+            case R.id.pause_download:
+                downloadBinder.pauseDownload();
+                break;
+            case R.id.cancel_download:
+                downloadBinder.cancelDownload();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "请同意权限",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(serviceConnection);
+    }
+}
