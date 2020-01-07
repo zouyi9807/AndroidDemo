42fc80c5ef259fd2a7b9a237a22cb00f50b0d148
zouyi9807 <1530193044@qq.com>
2020-01-08 04:41:15 +0800

    AppWeiget

---
+++ AppWeiget/app/src/main/java/com/example/zouyi/appweiget/Widget.java
@@ -0,0 +1,87 @@
+package com.example.zouyi.appweiget;
+
+import android.appwidget.AppWidgetProvider;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+/**
+ * Created by zouyi
+ * on 2019/5/31.
+ */
+public class Widget extends AppWidgetProvider {
+    public static final String CLICK_ACTION = "com.example.zouyi.appweiget.action.CLICK"; // 点击事件的广播ACTION
+
+    /**
+     * 每次窗口小部件被更新都调用一次该方法
+     */
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+        Intent intent = new Intent(CLICK_ACTION);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, R.id.imageView, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        remoteViews.setOnClickPendingIntent(R.id.imageView, pendingIntent);
+
+        for (int appWidgetId : appWidgetIds) {
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+        }
+    }
+
+    /**
+     * 接收窗口小部件点击时发送的广播
+     */
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        if (CLICK_ACTION.equals(intent.getAction())) {
+            Toast.makeText(context, "AppWidget", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * 每删除一次窗口小部件就调用一次
+     */
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+    }
+
+    /**
+     * 当最后一个该窗口小部件删除时调用该方法
+     */
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+
+    /**
+     * 当该窗口小部件第一次添加到桌面时调用该方法
+     */
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    /**
+     * 当小部件大小改变时
+     */
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+    }
+
+    /**
+     * 当小部件从备份恢复时调用该方法
+     */
+    @Override
+    public void onRestored(Context context, int[] oldWidgetIds, int[] newWidgetIds) {
+        super.onRestored(context, oldWidgetIds, newWidgetIds);
+    }
+}
